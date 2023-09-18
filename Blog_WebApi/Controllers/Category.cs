000e471310aef6e71b456463c8dc0aee33102af8
unknown <alirezang7575@gmail.com>
2023-09-18 07:41:12 -0700

    edit method and delete method was added

--- Blog_WebApi/Controllers/Category.cs
+++ Blog_WebApi/Controllers/Category.cs
@@ -49,15 +49,17 @@ public class Category : ControllerBase
 
         return lst;
     }
-[HttpGet]
-[Route("category/{id:guid}")]
-    public async ValueTask<IActionResult> getbyid([FromRoute]Guid id)
+
+    [HttpGet]
+    [Route("category/{id:guid}")]
+    public async ValueTask<IActionResult> getbyid([FromRoute] Guid id)
     {
         var category = _context.categoryuw.Getbyid(id);
-        if (category==null)
+        if (category == null)
         {
             return NoContent();
         }
+
         CategoriesList c = new()
         {
             id = category.id,
@@ -65,5 +67,42 @@ public class Category : ControllerBase
             urlhadle = category.urlhadle
         };
         return Ok(c);
+    }
+
+    [HttpPut]
+    [Route("catregoryedit/{id:guid}")]
+    public async ValueTask<IActionResult> updatecategory([FromRoute] Guid id, CategoriesList c)
+    {
+        try
+        {
+            category cat = new()
+            {
+                id = id,
+                name = c.name,
+                urlhadle = c.urlhadle
+            };
+            _context.categoryuw.update(cat);
+            _context.save();
+            return Ok();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            return BadRequest(e);
+        }
+    }
+
+    [HttpDelete]
+    [Route("catregoryedit/{id:guid}")]
+    public async ValueTask<IActionResult> deletecategory(Guid id)
+    {
+        if (id == null)
+        {
+            return BadRequest();
+        }
+
+        _context.categoryuw.deletebyid(id);
+        _context.save();
+        return Ok();
     }
 }
